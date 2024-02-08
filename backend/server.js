4882423e859acd89ba68e648b9f533df28eb036e
Sujithkumarvelpula <113710355+Sujithkumarvelpula@users.noreply.github.com>
2024-02-09 01:57:28 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://talk-a-tive-6dfv.onrender.com",
+    origin: "*",
   },
 });
 io.on("connection", (socket) => {
